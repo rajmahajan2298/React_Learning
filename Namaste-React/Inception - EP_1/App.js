42442a60ab9f669bf21d0d5359e04f7009fbc708
Raj Mahajan <programming2298@gmail.com>
2024-09-24 19:35:09 +0530

    Updated Files
    Updated In App.js In Which I have learned how to use nested element in React

--- Namaste-React/Inception - EP_1/App.js
+++ Namaste-React/Inception - EP_1/App.js
@@ -1,5 +1,14 @@
-const heading = React.createElement("h1", {id: "heading"}, "Hello World From React");
+// const heading = React.createElement("h1", {id: "heading"}, "Hello World From React");
 
-const root = ReactDom.createRoot(document.getElementById("root"));
+// const root = ReactDom.createRoot(document.getElementById("root"));
 
-root.render(heading);
+// root.render(heading);
+
+// Nested React Elements
+
+const parent = React.createElement("div", { id: "parent" },
+    React.createElement("div", { id: "child" },
+        React.createElement("h1", {}, "I'm an H1 Tag")));
+
+// If we have multiple Sibling or Child Elements we need to Use Array or List.
+
