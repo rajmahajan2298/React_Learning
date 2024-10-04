7bc49042218e1fe3386347c1ad2fcca0f084bbf4
Raj Mahajan <programming2298@gmail.com>
2024-10-04 19:42:21 +0530

    Learn About Why We use State variable and Search Functionality

--- Namaste-React/Exploring the World - EP_6/src/components/Header.js
+++ Namaste-React/Exploring the World - EP_6/src/components/Header.js
@@ -1,6 +1,10 @@
 import myImage from "../images/myReact.png";
+import { useState } from "react";
 
 const Header = () => {
+
+    const [btnNameReact, setBtnNameReact] = useState("Login");
+
     return (
         <div className="header">
             <div className="logo-container">
@@ -14,6 +18,13 @@ const Header = () => {
                     <li>About Us</li>
                     <li>Contact Us</li>
                     <li>Cart</li>
+                    <button className="login"
+                        onClick={() => {
+                            setBtnNameReact(btnNameReact === "Login" ? "Logout" : "Login")
+                        }}
+                        >
+                            {btnNameReact}
+                    </button>
                 </ul>
             </div>
         </div>
